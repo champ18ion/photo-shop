0498337c659f48e98eebbcb56948a899d3fe6c93
champ18ion <113764445+champ18ion@users.noreply.github.com>
2023-05-18 11:47:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# photo-shop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-aohuuz)
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './Components/App';
+import {BrowserRouter as Router} from "react-router-dom"
+
+import {ContextProvider} from "./Context"
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+<ContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ContextProvider>
+);
